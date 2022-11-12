28de6b0bfc1354c18d90a0e206f7343d4445347f
Juanfyio <118061286+Juanfyio@users.noreply.github.com>
2022-11-12 11:16:44 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
-console.log("Mensaje desde el index");
+const user_routes = require('./user_routes');
 
-const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
+const app_routes= (app) => {
+/* http://localhost:3000/api/v1/users */
+    app.use('/api/v1/users', user_routes);
+};
 
-const app = express();
-const port = 3000;
-const app_routes= require('./src/routes/index')
-
-app.listen(port, () => console.log("Active port", 3000));
-app.get("/", (req,res)=>
-res.send("Bienvenido"));
-
-mongoose
-  .connect(process.env.STRING_CONNECTION)
-  .then(() => console.log("We are connected with mongo"))
-  .catch((error) => console.error(error));
-
-  app.use(express.json());
-  app_routes(app);
+module.exports = app_routes;
---
+++ user_routes.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const user_model = require("../models/user_models");
+const user_routes = express.Router();
+
+/* http://localhost:3000/api/v1/users/user */
+user_routes.post("/user", (req, res) => {
+  const user_new = user_model(req.body);
+  user_new
+    .save()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((error) => {
+      res.json({ message: error });
+    });
+});
+
+user_routes.get("/user", (req, res) => {
+  user_model
+    .find()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+user_routes.get("/user/:id", (req, res) => {
+  const { id } = req.params;
+  user_model
+    .findById()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+user_routes.put("/user/:id", (req, res) => {
+  const { id } = req.params;
+  const { username, lastname, age, document, email, adress, pets} = req.body;
+  user_model
+    .updateOne({ _id: id }, { $set: { username, lastname, age, document, email, adress, pets } })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+user_routes.delete("/user/:id", (req, res) => {
+    const { id } = req.params;
+  user_model
+    .remove({ _id: id })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+module.exports = user_routes;
